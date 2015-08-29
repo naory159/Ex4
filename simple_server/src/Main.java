e2dde3b669371e5ad55697c67785eac182af0a86
Travis <travis@travis-ci.org>
2015-08-29 21:56:02 +0300

    aaa

--- simple_server/src/Main.java
+++ simple_server/src/Main.java
@@ -11,24 +11,24 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        
-        //ServerJFrame serverJFrame = new ServerJFrame();
-        
-        //serverJFrame.setVisible(true);
-        
-        SimpleServer server = new SimpleServer(45000);
+	public static void main(String[] args) throws IOException {
+
+		ServerJFrame serverJFrame = new ServerJFrame();
+
+		serverJFrame.setVisible(true);
+
+		/*SimpleServer server = new SimpleServer(45000);
         new Thread(server).start();
         System.out.println("Server is waiting to connect");
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-        
-       //System.out.println("Stopping Server");
-       //server.stop();
+        }*/
 
-    }
+		//System.out.println("Stopping Server");
+		//server.stop();
+
+	}
 
 }
