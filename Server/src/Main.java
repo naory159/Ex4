309e425e0762624406a0dc9f8186e7735dce8b9c
Travis <travis@travis-ci.org>
2015-08-29 23:54:14 +0300

    aaaaaaa

---
+++ Server/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		ServerJFrame serverJFrame = new ServerJFrame();
+
+		serverJFrame.setVisible(true);
+	}
+
+}
